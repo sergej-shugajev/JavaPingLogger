ab8c7bb9b9f6d8898f6ee054c4643bf1e4d9733b
Sergej Shugajev <shugajev@gmail.com>
2020-04-12 17:09:34 +0300

    #0.3 Замена табов на пробелы.

--- src/logic/Logger.java
+++ src/logic/Logger.java
@@ -10,62 +10,62 @@ import logic.Ping.PingValue;
 /** ведение записи измерений пинга */
 public class Logger {
 
-	/** загловок для CSV файла */
-	final static String LOG_COLS_NAMES = "host;time;ping\n";
+    /** загловок для CSV файла */
+    final static String LOG_COLS_NAMES = "host;time;ping\n";
 
-	static LocalDateTime dateTimeStart;
-	static long rowsInLog;
-	static String logFile;
-		
-	/** имя лог файла для записи измерений пинга */
-	public static String getLogFile() { return logFile; }
-	
-	/** записано строк в лог файл */
-	public static long getRowsInLog() { return rowsInLog; }
-	
-	/** инициализация переменных */
-	public static void init() {
-		rowsInLog = 0;
-		if (Vars.isCheckOnlyCommand())
-			logFile = "<check only>";
-		else {
-			dateTimeStart = LocalDateTime.now();
-			logFile = "ping_" 
-					+ dateTimeStart.format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm"))
-					+ ".csv";
-		}
-	}
-	
-	/** начало записи в лог (создание файла и шапка) */
-	public static void begin() {
-		if (logFile == null)
-			init();
-		if (Vars.isCheckOnlyCommand())
-			return;
-		try (FileWriter fw = new FileWriter(logFile, false)) {
-			fw.write(LOG_COLS_NAMES);
-			fw.flush();
-    		rowsInLog = 1;
+    static LocalDateTime dateTimeStart;
+    static long rowsInLog;
+    static String logFile;
+        
+    /** имя лог файла для записи измерений пинга */
+    public static String getLogFile() { return logFile; }
+    
+    /** записано строк в лог файл */
+    public static long getRowsInLog() { return rowsInLog; }
+    
+    /** инициализация переменных */
+    public static void init() {
+        rowsInLog = 0;
+        if (Vars.isCheckOnlyCommand())
+            logFile = "<check only>";
+        else {
+            dateTimeStart = LocalDateTime.now();
+            logFile = "ping_" 
+                    + dateTimeStart.format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm"))
+                    + ".csv";
+        }
+    }
+    
+    /** начало записи в лог (создание файла и шапка) */
+    public static void begin() {
+        if (logFile == null)
+            init();
+        if (Vars.isCheckOnlyCommand())
+            return;
+        try (FileWriter fw = new FileWriter(logFile, false)) {
+            fw.write(LOG_COLS_NAMES);
+            fw.flush();
+            rowsInLog = 1;
         }
         catch (IOException ex) {} 
-	}
+    }
 
-	/** сохранение всех хостов в лог */
-	public static void saveAllPings() {
-		if (Vars.isCheckOnlyCommand())
-			return;
-		if (rowsInLog == 0)
-			begin();
-		try (FileWriter fw = new FileWriter(logFile, true)) {
-			for(PingValue pv : Ping.getHostList()) {
-				fw.write(pv.getHost() + ";"
-						+ pv.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + ";"
-						+ pv.getPing() + "\n");
-	    		rowsInLog++;
-			}
-			fw.flush();
+    /** сохранение всех хостов в лог */
+    public static void saveAllPings() {
+        if (Vars.isCheckOnlyCommand())
+            return;
+        if (rowsInLog == 0)
+            begin();
+        try (FileWriter fw = new FileWriter(logFile, true)) {
+            for(PingValue pv : Ping.getHostList()) {
+                fw.write(pv.getHost() + ";"
+                        + pv.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + ";"
+                        + pv.getPing() + "\n");
+                rowsInLog++;
+            }
+            fw.flush();
         }
         catch (IOException ex) {} 
-	}
+    }
 
 }
